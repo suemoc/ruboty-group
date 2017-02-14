dafcb179eee0443d69a684baec85445a0e6838be
Yohei Susa <suemocs@gmail.com>
2017-02-14 11:13:35 +0900

    refactor

--- lib/ruboty/handlers/group.rb
+++ lib/ruboty/handlers/group.rb
@@ -1,16 +1,16 @@
 module Ruboty
   module Handlers
     class Group < Base
-      on /group\s+(.*)\s+(.*)/i, name: 'group', description: 'Return groups from member list'
+      on /group\s+(?<group_num>\d+)\s+(?<members>.+)/i, name: 'group', description: 'Return groups from member list'
 
       def group(message)
-        group_num = message[0]
-        members = message[1].split(',').shuffle!
+        group_num = message[:group_num].to_i
+        members = message[:members].split(',').shuffle!
 
-        groups = group_num.times.map{|i| members[members.size*i/group_num ... members.size*(i+1)/group_num]}
+        groups = group_num.times.map{|i| members[members.size*i/group_num ... members.size*(i+1)/group_num]}.reject(&:empty?)
         
         groups.each_with_index do |group, index|
-          message.reply("# -- group #{index} --\n#{group.join("\n")}\n")
+          message.reply("-- group #{index + 1} --\n#{group.join("\n")}\n")
         end
       end
     end
